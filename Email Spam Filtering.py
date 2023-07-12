7b64b129a8fd7438fe9f343a485299a26d127e89
Sumit Sunil Bavadekar <126313851+sumitbavadekar11@users.noreply.github.com>
2023-07-13 01:59:08 +0530

    Email Spam Filtering.py

--- Email Spam Filtering.py
+++ Email Spam Filtering.py
@@ -5,24 +5,21 @@ from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 
 def is_spam(email):
-    # Defining the stopwords to be removed
     stop_words = set(stopwords.words('english'))
     
-    # Tokenizing the email content
+   
     tokens = word_tokenize(email.lower())
     
-    # Removing stopwords from the tokens
+   
     filtered_tokens = [token for token in tokens if token not in stop_words]
     
-    # Checking if the email contains any spam-related keywords
+    
     spam_keywords = ['viagra', 'lottery', 'free', 'money', 'prize']
     for token in filtered_tokens:
         if token in spam_keywords:
             return True
     
     return False
-
-# Enter E-mail for email inputs
 email1 = input("Enter email 1: ")
 
 
